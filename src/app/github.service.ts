3834eb6162457350dfc94f5cbd53d534a72cb707
mbalanya <okobacharles@yahoo.co.uk>
2020-06-21 23:47:54 +0300

    get api key and add it to service

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -9,7 +9,9 @@ import { map } from 'rxjs/operators';
 
 export class GithubService {
 
-  
+  private username = 'mbalanya';
+  private client_id = 'a816b02129d1ad62c2bb';
+  private client_secret = '3a665e0224f6bab79c5107d6e49236e155c478fb';
 
   constructor(private _http:HttpClient) {
     console.log('Github Service init....!');
