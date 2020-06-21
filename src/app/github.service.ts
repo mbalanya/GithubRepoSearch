01376634b69e3cf449fe512975e5fa4ae4c8752f
mbalanya <okobacharles@yahoo.co.uk>
2020-06-21 23:47:12 +0300

    link service to api

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -5,4 +5,21 @@ import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
-})
+});
+
+export class GithubService {
+
+  
+
+  constructor(private _http:HttpClient) {
+    console.log('Github Service init....!');
+
+  }
+
+  getUser(){
+    return this._http.get('https://api.github.com/users/' + this.username)
+      //.pipe(map((response: any) => response.json()));
+      .pipe(map(res => res ));
+  }
+
+}
