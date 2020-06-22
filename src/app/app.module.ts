c8dbdc4c3f16c8aea302ba38a15a376a5d4fcbb0
mbalanya <okobacharles@yahoo.co.uk>
2020-06-22 03:32:47 +0300

    create homepage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubComponent
+    GithubComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
